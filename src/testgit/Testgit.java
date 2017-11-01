943b23631f05d2ec88f9f1bd86637fcbbba2048c
B1 <B1@B149>
2017-11-01 13:15:42 +0800

    first test

--- src/testgit/Testgit.java
+++ src/testgit/Testgit.java
@@ -17,6 +17,7 @@ public class Testgit {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test3");
     }
     
 }
